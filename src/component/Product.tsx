8f7a77ed075b4374e750ff1c5ae525c2eb4bfcda
Olha Zemliakova <zemlakova@gmail.com>
2023-03-11 07:37:38 -0800

    Created Product component

---
+++ src/component/Product.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import { IProduct } from "../modeles"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: ProductProps) {
+    const [details, setDetails] = useState(false)
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+    const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
+    return (
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            <img src={props.product.image} className="w-1/6" alt="props.product.title" />
+            <p>{props.product.title}</p>
+            <span className="font-bold">{props.product.price}</span>
+            <button className={btnClasses.join(' ')}
+                onClick={() => setDetails(prev => !prev)}
+            >
+                {details ? "Hide details" : "Show details"}
+            </button>
+            {details &&
+                <div>
+                    <p>{props.product.description}</p>
+                    <p>Rate: <span style={{ fontWeight: 'bold' }}>{props.product.rating.rate}</span></p>
+                </div>}
+        </div>
+    )
+}
