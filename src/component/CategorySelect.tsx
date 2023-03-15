0597263493fc91055963b6b84fbea6f07f070786
Olha Zemliakova <zemlakova@gmail.com>
2023-03-15 08:14:45 -0700

    Created select component

---
+++ src/component/CategorySelect.tsx
@@ -0,0 +1,14 @@
+import Form from 'react-bootstrap/Form';
+
+export function CategorySelect() {
+
+    return (
+        <Form.Select size="sm" aria-label="Default select example">
+      <option>Please choose a category</option>
+      <option value="men-clothes">Men's clothes</option>
+      <option value="women's clothing">Women's clothing</option>
+      <option value="jewellery">Jewellery</option>
+      <option value="electronics">Electronics</option>
+    </Form.Select>
+    )
+}
